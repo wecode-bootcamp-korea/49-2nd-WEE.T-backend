435b442ffb5ef750cfd0ee07575e3b2d8bdefb13
JeYeongR <wpdud2003@gmail.com>
2023-10-14 22:04:13 +0900

    feat: 카카오 소셜 로그인 API에 리프레시 토큰 추가

--- src/config/passport.js
+++ src/config/passport.js
@@ -10,26 +10,23 @@ const kakaoStrategyConfig = new KakaoStrategy(
     callbackURL: process.env.KAKAO_CALLBACK_URL,
   },
   async (accessToken, refreshToken, profile, done) => {
-    try {
-      const email = profile._json.kakao_account.email;
-      const snsId = profile.id;
-      const socialId = 1;
+    const email = profile._json.kakao_account.email;
+    const snsId = profile.id;
+    const socialId = 1;
 
-      const existingUser = await userDao.findUserBySNS(snsId, socialId);
+    const existingUser = await userDao.findUserBySNS(snsId, socialId);
 
-      let id = existingUser?.id;
+    let id = existingUser?.id;
 
-      if (!existingUser) {
-        const result = await userDao.createUser(email, snsId, socialId);
-        id = result.insertId;
-      }
-
-      const token = jwt.sign({ id }, process.env.SECRET_KEY);
-
-      return done(null, token);
-    } catch (err) {
-      return done(err);
+    if (!existingUser) {
+      const result = await userDao.createUser(email, snsId, socialId);
+      id = result.insertId;
     }
+
+    accessToken = jwt.sign({ id }, process.env.SECRET_KEY, { expiresIn: "1h", issuer: "JeYeong" });
+    refreshToken = jwt.sign({}, process.env.SECRET_KEY, { expiresIn: "14d", issuer: "JeYeong" });
+
+    return done(null, { accessToken, refreshToken });
   }
 );
 
--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,9 +1,11 @@
 const socialLogin = async (req, res) => {
   const token = req.user;
 
+  console.log(token);
   res.status(200).json({
     message: "LOGIN_SUCCESS",
-    accessToken: token,
+    accessToken: token.accessToken,
+    refreshToken: token.refreshToken,
   });
 };
 
