5873814f53112beffdd54cbf15f893a4e604cb16
JeYeongR <wpdud2003@gmail.com>
2023-10-15 15:35:18 +0900

    test: 회원정보 수정 API 테스트 코드 추가

--- src/models/index.js
+++ src/models/index.js
@@ -1,9 +1,11 @@
 const userDao = require("./userDao");
 const subscribeDao = require("./subscribeDao");
 const healthInfoDao = require("./healthInfoDao");
+const genderDao = require("./genderDao");
 
 module.exports = {
   userDao,
   subscribeDao,
   healthInfoDao,
+  genderDao,
 };
