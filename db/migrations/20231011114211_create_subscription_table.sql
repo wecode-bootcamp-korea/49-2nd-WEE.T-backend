999dce67195d5daeb26502c92af54b59a2a2456d
B3lla <sjin09300@gmail.com>
2023-10-18 00:51:30 +0900

    chore: dbmate subscription 수정

--- db/migrations/20231011114211_create_subscription_table.sql
+++ db/migrations/20231011114211_create_subscription_table.sql
@@ -1,6 +1,6 @@
 -- migrate:up
 CREATE TABLE `subscription` (
-  `id` int PRIMARY KEY NOT NULL,
+  `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `month` varchar(4) NOT NULL,
   `price` int NOT NULL
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
