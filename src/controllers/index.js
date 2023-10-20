d108260de629db7d92933b5c4eac277f5dc037ff
JeYeongR <wpdud2003@gmail.com>
2023-10-20 16:09:00 +0900

    feat: 서비스 정보 조회 API 구현

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,7 +1,9 @@
 const authController = require("./authController");
 const userController = require("./userController");
+const statisticsController = require("./statisticsController");
 
 module.exports = {
   authController,
   userController,
+  statisticsController,
 };
