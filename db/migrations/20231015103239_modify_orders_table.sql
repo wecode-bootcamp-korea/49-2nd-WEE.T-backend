c7fb1da3422c904af788f9c1d889dce37ac12b7e
B3lla <sjin09300@gmail.com>
2023-10-23 14:42:17 +0900

    chore: dbmate orders 테이블 수정

--- db/migrations/20231015103239_modify_orders_table.sql
+++ db/migrations/20231015103239_modify_orders_table.sql
@@ -3,8 +3,8 @@ SET foreign_key_checks = 0;
 ALTER TABLE orders CHANGE id id int NOT NULL AUTO_INCREMENT;
 ALTER TABLE orders CHANGE created_at created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP;
 ALTER TABLE orders CHANGE updated_at updated_at timestamp NULL ON UPDATE CURRENT_TIMESTAMP;
-ALTER TABLE `orders` ADD `order_id` int NOT NULL;
-ALTER TABLE `orders` ADD `subscribes_id` int NOT NULL;
+ALTER TABLE `orders` ADD `order_id` int NOT NULL UNIQUE;
+ALTER TABLE `orders` ADD `subscribes_id` int NOT NULL UNIQUE;
 ALTER TABLE `orders` ADD FOREIGN KEY (`subscribes_id`) REFERENCES `subscribes_id` (`id`);
 SET foreign_key_checks = 1;
 
